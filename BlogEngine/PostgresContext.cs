59d906760358652245d9ef92ec3bf2980e0e17f6
Austin <Borzen00@gmail.com>
2018-06-20 20:12:20 -0500

    adding in postgres dotnetcore switchout

---
+++ BlogEngine/PostgresContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BlogEngine
+{
+    public class PostgresContext : DbContext
+    {
+        public PostgresContext(DbContextOptions<PostgresContext> options)
+        {
+
+        }
+    }
+}
